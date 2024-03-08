458da11ab2674e67b992b3d6509580f5bf7c9156
FosterClark48 <fosterclark48@yahoo.com>
2024-03-08 14:22:57 -0600

    put this into it's own directory to separate programs

---
+++ MyNewProgram.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyApplication
+{
+    class MyNewProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my new program!");
+        }
+    }
+}
